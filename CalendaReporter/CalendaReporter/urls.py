44032502eecf5d5b9489aef8d6fe401183eed34e
igorvukajlovic <vukajlovic.igor@hotmail.it>
2022-07-12 17:02:07 +0200

    Quasi MVC

--- CalendaReporter/CalendaReporter/urls.py
+++ CalendaReporter/CalendaReporter/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
-from . import views
+from .Controllers import CalendarsController, CalendarController
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index),
-    path('post/<int:id>/', views.individual_post),
+    path('', CalendarsController.get),
+    path('Calendars/', CalendarsController.get),
+    path('Calendars/<str:name>/', CalendarController.get),
 ]
--- CalendaReporter/CalendaReporter/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse('Hello, welcome to the index page.')
-
-def individual_post(request, id):
-    return HttpResponse('Hi, this is where an individual post will be.' + str(id))
