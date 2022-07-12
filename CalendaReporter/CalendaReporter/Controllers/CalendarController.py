6314ad54ad089f8ac0b9358a9ef18018cda82b45
igorvukajlovic <vukajlovic.igor@hotmail.it>
2022-07-12 18:55:38 +0200

    Created Calendar Model

--- CalendaReporter/CalendaReporter/Controllers/CalendarController.py
+++ CalendaReporter/CalendaReporter/Controllers/CalendarController.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from CalendaReporter.Models.MCalendar import Calendar
 
-def get(request, name):
-    return HttpResponse('Info calendario ' + name)
+def get(request, name, rangeFrom, rangeTo):
+    calendario = Calendar(name)
+    ore_lavorate = calendario.countHoursInRange()
+    return HttpResponse('Info calendario ' + calendario.name + ". Hai lavorato " + str(ore_lavorate) + " ore.")
---
+++ CalendaReporter/CalendaReporter/Models/MCalendar.py
@@ -0,0 +1,7 @@
+class Calendar:
+    def __init__(self, name):
+        self.name = name
+
+    def countHoursInRange(self):
+        return 2
+        
